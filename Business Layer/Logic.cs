c2f235ff40d1729fea5b5390b92532901ecf5283
Chrissy-22 <christine.jeanethomas22@gmail.com>
2024-11-05 20:43:43 +0200

    Implemented the three-tier architecture by creating a Presentation Layer, Business Layer and Data Layer

---
+++ Business Layer/Logic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behr_Singo_Thomas_Veldman_PRG282_Project.Business_Layer
+{
+    internal class Logic
+    {
+    }
+}
